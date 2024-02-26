0f216ffa9d48cb58ad1ffdfc309ad22f909d8dad
ultr4 <cl.rch09@icloud.com>
2024-02-26 16:00:06 -0300

    :recycle: refactor: Add ValidationException and MethodArgumentNotValidException to throw Unprocessable_Entity status code

--- src/main/java/com/rinhabackend/rinhadebackend2024q1/exception/GlobalExceptionHandler.java
+++ src/main/java/com/rinhabackend/rinhadebackend2024q1/exception/GlobalExceptionHandler.java
@@ -2,8 +2,11 @@ package com.rinhabackend.rinhadebackend2024q1.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import jakarta.validation.ValidationException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -13,8 +16,9 @@ public class GlobalExceptionHandler {
     return new ResponseEntity<>(HttpStatus.NOT_FOUND);
   }
 
-  @ExceptionHandler(ClienteLimitExceededException.class)
-  public ResponseEntity<Void> handleClienteLimitExceeded(ClienteLimitExceededException ex) {
+  @ExceptionHandler({ ClienteLimitExceededException.class, ValidationException.class,
+      MethodArgumentNotValidException.class })
+  public ResponseEntity<Void> handleClienteLimitExceeded(Exception ex) {
     return new ResponseEntity<>(HttpStatus.UNPROCESSABLE_ENTITY);
   }
 }
