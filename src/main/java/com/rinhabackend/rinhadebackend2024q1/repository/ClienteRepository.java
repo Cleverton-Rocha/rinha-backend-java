ace5b7c266b1fffaeb8f52a90218cd885f8ed8c9
ultr4 <cl.rch09@icloud.com>
2024-02-26 15:52:28 -0300

    :sparkles: feat: Add pessimistic lock custom query for table Cliente

--- src/main/java/com/rinhabackend/rinhadebackend2024q1/repository/ClienteRepository.java
+++ src/main/java/com/rinhabackend/rinhadebackend2024q1/repository/ClienteRepository.java
@@ -1,10 +1,17 @@
 package com.rinhabackend.rinhadebackend2024q1.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 
 import com.rinhabackend.rinhadebackend2024q1.domain.Cliente;
 
+import jakarta.persistence.LockModeType;
+
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+  @Lock(LockModeType.PESSIMISTIC_WRITE)
+  Optional<Cliente> findClienteById(Long id);
 }
