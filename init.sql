e519b2ba218bb532037a2f146c7480a6bc614b6d
ultr4 <cl.rch09@icloud.com>
2024-02-26 15:48:45 -0300

    :wrench: chore: Create init.sql

--- init.sql
+++ init.sql
@@ -1,21 +1,19 @@
 CREATE EXTENSION IF NOT EXISTS pg_prewarm;
 
-CREATE TYPE TipoTransacao AS ENUM ('c', 'd');
-
 CREATE TABLE IF NOT EXISTS cliente (
   id SERIAL PRIMARY KEY,
   nome VARCHAR(50) NOT NULL,
-  limite INT DEFAULT 0,
-  saldo INT DEFAULT 0
+  limite INT DEFAULT 0 NOT NULL,
+  saldo INT DEFAULT 0 NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS transacao (
   id SERIAL PRIMARY KEY,
   id_cliente INT,
-  valor INT DEFAULT 0,
-  tipo TipoTransacao,
-  descricao VARCHAR(10),
-  realizada_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  valor INT DEFAULT 0 NOT NULL,
+  tipo CHAR(1) NOT NULL,
+  descricao VARCHAR(10) NOT NULL,
+  realizada_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
   FOREIGN KEY (id_cliente) REFERENCES cliente(id)
 );
 
