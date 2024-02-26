4e5dd104b11526b27f110d97f73d91836a80f1dd
ultr4 <cl.rch09@icloud.com>
2024-02-26 15:51:34 -0300

    :sparkles: feat: Add custom query for TransacaoRepository

--- src/main/java/com/rinhabackend/rinhadebackend2024q1/repository/TransacaoRepository.java
+++ src/main/java/com/rinhabackend/rinhadebackend2024q1/repository/TransacaoRepository.java
@@ -1,5 +1,8 @@
 package com.rinhabackend.rinhadebackend2024q1.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Limit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +10,5 @@ import com.rinhabackend.rinhadebackend2024q1.domain.Transacao;
 
 @Repository
 public interface TransacaoRepository extends JpaRepository<Transacao, Long> {
+  List<Transacao> findByClienteIdOrderByRealizadaEmDesc(Long id, Limit limit);
 }
