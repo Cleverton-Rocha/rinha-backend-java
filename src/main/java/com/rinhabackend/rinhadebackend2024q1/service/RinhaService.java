835951e1936c308f88fb38926ed2f7478e7ef6d4
ultr4 <cl.rch09@icloud.com>
2024-02-26 16:02:45 -0300

    :sparkles: feat: Create CriarTransacao method

--- src/main/java/com/rinhabackend/rinhadebackend2024q1/service/RinhaService.java
+++ src/main/java/com/rinhabackend/rinhadebackend2024q1/service/RinhaService.java
@@ -1,15 +1,20 @@
 package com.rinhabackend.rinhadebackend2024q1.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Limit;
 import org.springframework.stereotype.Service;
 
 import com.rinhabackend.rinhadebackend2024q1.domain.Cliente;
+import com.rinhabackend.rinhadebackend2024q1.domain.Transacao;
 import com.rinhabackend.rinhadebackend2024q1.exception.ClienteLimitExceededException;
 import com.rinhabackend.rinhadebackend2024q1.exception.ClienteNotFoundException;
-import com.rinhabackend.rinhadebackend2024q1.model.TipoTransacao;
+import com.rinhabackend.rinhadebackend2024q1.model.ExtratoResponse;
+import com.rinhabackend.rinhadebackend2024q1.model.ExtratoTransacao;
+import com.rinhabackend.rinhadebackend2024q1.model.Saldo;
 import com.rinhabackend.rinhadebackend2024q1.model.TransacaoRequest;
 import com.rinhabackend.rinhadebackend2024q1.model.TransacaoResponse;
 import com.rinhabackend.rinhadebackend2024q1.repository.ClienteRepository;
@@ -32,29 +37,55 @@ public class RinhaService {
 
   @Transactional
   public TransacaoResponse transacao(Long id, TransacaoRequest request) {
-    Optional<Cliente> clienteExists = clienteRepository.findById(id);
+    Cliente cliente = clienteRepository.findClienteById(id)
+        .orElseThrow(ClienteNotFoundException::new);
 
-    if (clienteExists.isEmpty())
-      throw new ClienteNotFoundException();
+    char tipo = request.tipo().charAt(0);
 
-    Cliente cliente = clienteExists.get();
-
-    TipoTransacao tipo = request.tipo();
-
-    if (tipo == TipoTransacao.c) {
+    if (tipo == 'c') {
       cliente.realizarCredito(request.valor().intValue());
     }
 
-    if (tipo == TipoTransacao.d) {
-      if (cliente.getSaldo() - request.valor() < -cliente.getLimite()) {
+    if (tipo == 'd') {
+      if (cliente.getSaldo() - request.valor().intValue() < -cliente.getLimite()) {
         throw new ClienteLimitExceededException();
       }
       cliente.realizarDebito(request.valor().intValue());
     }
 
+    Transacao transacao = criarTransacao(request, cliente, tipo);
+    cliente.criarTransacao(transacao);
+    clienteRepository.save(cliente);
     TransacaoResponse response = new TransacaoResponse(cliente.getLimite(), cliente.getSaldo());
 
     return response;
+  }
+
+  @Transactional
+  public ExtratoResponse extrato(Long id) {
+    Cliente cliente = clienteRepository.findById(id)
+        .orElseThrow(ClienteNotFoundException::new);
+
+    Saldo saldo = new Saldo(cliente.getSaldo(), LocalDateTime.now(), cliente.getLimite());
+    List<ExtratoTransacao> transacoes = transacaoRepository
+        .findByClienteIdOrderByRealizadaEmDesc(id, Limit.of(10)).stream()
+        .map(t -> new ExtratoTransacao(
+            t.getValor(), t.getTipo(), t.getDescricao(), t.getRealizadaEm()))
+        .toList();
+
+    ExtratoResponse extrato = new ExtratoResponse(saldo, transacoes);
+
+    return extrato;
+  }
+
+  private Transacao criarTransacao(TransacaoRequest request, Cliente cliente, char tipo) {
+    Transacao transacao = new Transacao();
+    transacao.setCliente(cliente);
+    transacao.setTipo(tipo);
+    transacao.setValor(request.valor().intValue());
+    transacao.setDescricao(request.descricao());
+    transacao.setRealizadaEm(LocalDateTime.now());
+    return transacao;
   }
 
 }
