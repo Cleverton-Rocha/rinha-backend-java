8c6012d6b2760ac39e81360dd24f66a7e698dd2f
ultr4 <cl.rch09@icloud.com>
2024-02-26 16:01:03 -0300

    :recycle: refactor: Change valor validation requirement

--- src/main/java/com/rinhabackend/rinhadebackend2024q1/model/TransacaoRequest.java
+++ src/main/java/com/rinhabackend/rinhadebackend2024q1/model/TransacaoRequest.java
@@ -1,12 +1,17 @@
 package com.rinhabackend.rinhadebackend2024q1.model;
 
+import java.math.BigDecimal;
+
+import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.constraints.Size;
 
 public record TransacaoRequest(
-    @NotNull Integer valor,
-    @NotEmpty TipoTransacao tipo,
-    @NotBlank @Size(max = 10) String descricao) {
+                @NotNull @PositiveOrZero @Digits(integer = 12, fraction = 0) BigDecimal valor,
+                @NotEmpty @Pattern(regexp = "[cd]") String tipo,
+                @NotBlank @Size(max = 10) String descricao) {
 }
